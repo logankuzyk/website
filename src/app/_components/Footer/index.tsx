a559b6c6b0d01a28259015b3b770a01a44d95aff
Logan Kuzyk <logan@kuzyk.net>
2024-10-26 14:05:18 -0700

    Customize navbar and footer

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -26,32 +26,13 @@ export async function Footer() {
   return (
     <footer className={classes.footer}>
       <Gutter className={classes.wrap}>
-        <Link href="/">
-          <picture>
-            <img
-              className={classes.logo}
-              alt="Payload Logo"
-              src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
-            />
-          </picture>
-        </Link>
         <nav className={classes.nav}>
-          <ThemeSelector />
           {navItems.map(({ link }, i) => {
             return <CMSLink key={i} {...link} />
           })}
-          <Link href="/admin">Admin</Link>
-          <Link
-            href="https://github.com/payloadcms/payload/tree/main/templates/website"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source Code
-          </Link>
-          <Link href="https://payloadcms.com" target="_blank" rel="noopener noreferrer">
-            Payload
-          </Link>
+          <ThemeSelector />
         </nav>
+        <div>&copy; Logan Kuzyk, 2024</div>
       </Gutter>
     </footer>
   )
